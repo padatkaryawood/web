7ed4ca03ced78b7b149cf34c84ff41bff63168d2
padatkaryawood <padatkaryawood@gmail.com>
2025-07-08 23:47:03 +0700

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const SECRET = "RYZORT_SECRET";
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "No token provided" });
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: "Invalid token" });
+  }
+};
